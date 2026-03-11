89a4bbd60055328652c25f294d3a258426510f21
Shreyas <shreyasnaik81@gmail.com>
2026-03-11 23:08:34 +0530

    Refactor forest_fire example to use NumPy array for environment state

--- examples/forest_fire/app.py
+++ examples/forest_fire/app.py
@@ -9,33 +9,41 @@ from mesa.visualization.user_param import (
 )
 
 COLORS = {"Fine": "#00AA00", "On Fire": "#880000", "Burned Out": "#000000"}
-
+from forest_fire.model import FINE, ON_FIRE, BURNED_OUT, EMPTY
+COLORS_BY_STATE = {
+    EMPTY: "#FFFFFF",
+    FINE: COLORS["Fine"],
+    ON_FIRE: COLORS["On Fire"],
+    BURNED_OUT: COLORS["Burned Out"]
+}
 
 def forest_fire_portrayal(tree):
     if tree is None:
         return
-    portrayal = {"Shape": "rect", "w": 1, "h": 1, "Filled": "true", "Layer": 0}
-    (x, y) = (tree.cell.coordinate[i] for i in (0, 1))
+        
+    portrayal = {
+        "Shape": "rect",
+        "w": 1,
+        "h": 1,
+        "Filled": "true",
+        "Layer": 0
+    }
+
+    x, y = tree.cell.coordinate
+    state = tree.model.fire_state[x, y]
+    
     portrayal["x"] = x
     portrayal["y"] = y
-    portrayal["color"] = COLORS[tree.condition]
+    portrayal["color"] = COLORS_BY_STATE[state]
+
     return portrayal
-
-
-def post_process_space(ax):
-    ax.set_aspect("equal")
-    ax.set_xticks([])
-    ax.set_yticks([])
-
 
 def post_process_lines(ax):
     ax.legend(loc="center left", bbox_to_anchor=(1, 0.9))
 
-
 space_component = make_space_component(
     forest_fire_portrayal,
     draw_grid=False,
-    post_process=post_process_space,
 )
 lineplot_component = make_plot_component(
     COLORS,
